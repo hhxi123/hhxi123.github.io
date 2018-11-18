6c47514403e817c703542d8e7fa5c9314f4ef952
hhxi123 <43728638+hhxi123@users.noreply.github.com>
2018-11-18 15:09:23 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ window.onload = function() {
     center: [39.0997, -94.5786],
     layers: L.mapquest.tileLayer('map'),
     zoom: 10,
-    boxZoom._enabled: false,
+    boxZoom._enabled: falses
   });
 
   console.log(map);
